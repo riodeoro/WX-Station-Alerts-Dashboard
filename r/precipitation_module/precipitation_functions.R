10e6fd945a1862ac29122ba68b202f6ddf519309
riodeoro <Jake.Lee@gov.bc.ca>
2025-01-14 12:12:55 -0800

    Update precipitation_functions.R

--- r/precipitation_module/precipitation_functions.R
+++ r/precipitation_module/precipitation_functions.R
@@ -1,3 +1,18 @@
+# Copyright 2025 Province of British Columbia
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# r/precipitation_module/precipitation_functions.R
+
 #RN_1 OUTLIERS FUNCTION
 identify_outliers <- function(data, k = 6, threshold_multiplier = 4) {
   if (!"Rn_1" %in% colnames(data)) {

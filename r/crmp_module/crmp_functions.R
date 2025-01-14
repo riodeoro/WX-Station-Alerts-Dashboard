9c95c24d2f84a3ead45e3b9308c97f9f9dbf7ab9
riodeoro <Jake.Lee@gov.bc.ca>
2025-01-14 12:06:52 -0800

    Update crmp_functions.R

--- r/crmp_module/crmp_functions.R
+++ r/crmp_module/crmp_functions.R
@@ -1,3 +1,19 @@
+#    Copyright 2025 Province of British Columbia
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+#    
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# crmp_functions.R
+
 identify_sdepth_concerns <- function(data, change_threshold = 10, negative_threshold = -2.0) {
   has_sdepth <- "SDepth" %in% colnames(data)
   has_sd <- "SD" %in% colnames(data)

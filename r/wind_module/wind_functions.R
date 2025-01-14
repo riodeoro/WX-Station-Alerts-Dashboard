9c07f50f80ee1cb553d65c52e6097dc249a0cd0e
riodeoro <Jake.Lee@gov.bc.ca>
2025-01-14 12:16:48 -0800

    Update wind_functions.R

--- r/wind_module/wind_functions.R
+++ r/wind_module/wind_functions.R
@@ -1,3 +1,18 @@
+# Copyright 2025 Province of British Columbia
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#r/wind_module/wind_functions.R
+
 get_most_recent_non_zero_wspd <- function(data) {
   # Early returns if data is invalid
   if (is.null(data) || nrow(data) == 0 || !"Wspd" %in% names(data)) {

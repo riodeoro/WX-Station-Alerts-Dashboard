74d2d3a13978ca38e4335b39eb8272741d008e48
riodeoro <Jake.Lee@gov.bc.ca>
2025-01-14 12:30:02 -0800

    Update app.R

--- app.R
+++ app.R
@@ -1,16 +1,3 @@
-# Copyright 2025 Province of British Columbia
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # app.R
 
 # ---------- Library Imports ----------
@@ -34,6 +21,7 @@ source("ui_definition.R")
 source("global_functions.R")
 source("global_resources.R")
 
+addResourcePath("www", "www")
 
 # Module function files
 source("r/db_module/db_functions.R")
